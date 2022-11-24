a311fdb3f6537c47e9e6a68107033c6242183f08
ndry0611 <andryss0611@gmail.com>
2022-11-24 19:42:44 +0700

    Redeploy

--- app.js
+++ app.js
@@ -9,4 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
